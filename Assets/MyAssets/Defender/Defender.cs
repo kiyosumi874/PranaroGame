1091834d21f5d2919fdff299374be524c2bc193b
hai-dare <!CWewRL3vYXydU7>
2022-12-03 10:08:32 +0900

    フォルダ位置調整、スタートシーン再調整、ディフェンダーの動き追加

---
+++ Assets/MyAssets/Defender/Defender.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Defender : MonoBehaviour
+{
+    //スピード（調整用）
+    [Tooltip("移動速度調整")]
+    [SerializeField] float speed;
+
+    [Tooltip("移動幅")]
+    [SerializeField] float maxMoved;
+
+    [Tooltip("再POPの時間")]
+    [SerializeField] float rePop;
+
+    //DefenderのRidid
+    [SerializeField] Rigidbody rigidbody;
+
+    //移動の方向を変更するための変数
+    int randSp =1;
+
+    //消滅管理のBool
+    bool checkDestroy = false;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+
+        randSp = Random.Range(-1, 1);
+        //0の場合は１へ（乗算するので、０だとまずい）
+        if (randSp==0)
+        {
+            randSp = 1;
+        }
+    }
+
+
+    //参考：https://3dcg-school.pro/unity-object-move-beginner/#Rigidbody
+    private void FixedUpdate()
+    {
+       var pos = rigidbody.transform.position;
+
+        //checkDestroyがONの時は処理を行わない
+        if (checkDestroy)
+        {
+            return;
+        }
+
+        //X座標がMAX値より小さい場合
+        if (pos.x < maxMoved)
+        {
+            //指定したスピードから現在の速度を引いて加速力を求める
+            float currentSpeed = speed - rigidbody.velocity.magnitude;
+            //調整された加速力で力を加える
+            rigidbody.AddForce(new Vector3(currentSpeed * randSp, 0, 0));
+
+
+        } //X座標がMAX値より大きい場合
+        else if (pos.x >= maxMoved)
+        {
+            //指定したスピードから現在の速度を引いて加速力を求める
+            float currentSpeed = speed - rigidbody.velocity.magnitude;
+            //調整された加速力で力を加える
+            rigidbody.AddForce(new Vector3(-currentSpeed * randSp, 0, 0));
+        }
+    }
+
+    //ボールに当たったら消滅
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag== "SoccerBall")
+        {
+            this.gameObject.SetActive(false);
+            checkDestroy = true;
+
+            var pos = transform.position;
+
+            //時間差で復活させる
+            DOVirtual.DelayedCall(rePop, ()=> { SetPopDefender(pos); }
+            );
+        }
+    }
+
+    private void SetPopDefender(Vector3 pos)
+    {
+        this.gameObject.SetActive(true);
+
+        this.gameObject.transform.position = pos;
+        checkDestroy = false;
+        Debug.Log("再POP");
+    }
+
+}
